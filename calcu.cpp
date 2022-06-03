fa8140e81b2995901eda3de1639e77312ed209ad
SKEGDEV <skegdeveloper@gmail.com>
2022-06-02 20:06:16 -0600

    v1

---
+++ calcu.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+float multiplication(int num1, int num2){
+  return num1 * num2;
+}
+
+float divition(int num1, int num2){
+  return num1 / num2;
+}
+
+int main(){
+
+  return 0;
+}
